8c3f0ba30a85da179a58a722575ead36975d9b51
Minh <ml007291@ohio.edu>
2022-09-07 17:40:59 -0400

    adding Hello Gitgit status to main

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Minh Le
+ *        Date: 09/07/2022
+ * Description: C++ program that will be used 
+ *              to test updating the online branch
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
